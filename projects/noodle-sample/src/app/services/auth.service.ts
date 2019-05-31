de43bfdec2e7bda9645a05e8c8a6d284e9d3777c
Noodle <ntn.f51@gmail.com>
2019-05-31 17:03:37 +0700

    show username/password after login

--- projects/noodle-sample/src/app/services/auth.service.ts
+++ projects/noodle-sample/src/app/services/auth.service.ts
@@ -3,11 +3,15 @@ import { Injectable, EventEmitter } from '@angular/core';
 @Injectable()
 export class AuthService {
   loginStatus: boolean = false;
+  username: string = '';
+  password: string = '';
   statusChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   logIn(username: string, password: string) {
     // call api to authenticate user
     this.loginStatus = true;
+    this.username = username;
+    this.password = password;
     this.statusChanged.emit(this.loginStatus);
   }
 

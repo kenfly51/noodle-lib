d3186b930116a337e3ba61688677205e101d6724
Noodle <ntn.f51@gmail.com>
2019-05-31 16:18:21 +0700

    clean code

--- projects/noodle-sample/src/app/components/app/app.component.ts
+++ projects/noodle-sample/src/app/components/app/app.component.ts
@@ -7,18 +7,13 @@ import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild(SideBarContainerComponent) sideBar: SideBarContainerComponent
 
   constructor(private router: Router, private authService: AuthService) {
 
-  }
-
-  toggleSidebar() {
-    this.sideBar.toggleSidebar();
   }
 
   checkLoginStatus(isLoggedIn) {
